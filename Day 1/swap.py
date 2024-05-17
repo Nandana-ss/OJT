135a34e37ce8d608e344cd61ce2d341f46a13780
Nandana-ss <122439326+Nandana-ss@users.noreply.github.com>
2024-05-17 12:36:29 +0530

    Update swap.py

--- Day 1/swap.py
+++ Day 1/swap.py
@@ -1,4 +1,4 @@
-# Write a Python program to swap two numbers without using a temporary variable.(15-05-2024)
+# Write a Python program to swap two numbers without using a temporary variable.(16-05-2024)
 a=int(input("enter the first number: "))
 b=int(input("enter the second number: "))
 print("unswapped first number: ",a)
