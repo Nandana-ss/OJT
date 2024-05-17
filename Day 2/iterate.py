f50acb42b334f0306d6bdd1d5d8f33e5663f6cfb
Nandana-ss <122439326+Nandana-ss@users.noreply.github.com>
2024-05-17 12:33:24 +0530

    Update iterate.py

--- Day 2/iterate.py
+++ Day 2/iterate.py
@@ -1,4 +1,4 @@
-#  Iterate over keys and values in a dictionary.(16-05-2024)
+#  Iterate over keys and values in a dictionary.(17-05-2024)
 dict = {'a': 1, 'b': 2, 'c': 3}
 for key, value in dict.items():
     print(key, value)
