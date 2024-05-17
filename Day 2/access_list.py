27e20b0e09f011f6e7965c494055a57e1bcf98d6
Nandana-ss <122439326+Nandana-ss@users.noreply.github.com>
2024-05-17 14:18:07 +0530

    Add files via upload

---
+++ Day 2/access_list.py
@@ -0,0 +1,4 @@
+# Create a tuple and access its elements. (16-05-2024)
+my_tuple = (1, 2, 3, 4, 5)
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
---
+++ Day 2/tuple_iterate.py
@@ -0,0 +1,4 @@
+# Iterate over a tuple and print its elements
+ituple = (10, 20, 30, 40, 50)
+for element in ituple:
+    print(element)
