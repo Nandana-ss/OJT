768cbd7bbb5b7dda026ffed23e9b267b3aa310ed
Nandana-ss <122439326+Nandana-ss@users.noreply.github.com>
2024-07-11 16:51:09 +0530

    Add files via upload

---
+++ Day 39/Act2.py
@@ -0,0 +1,12 @@
+# import itertools
+# def generate_permutations():
+#     letters=['a','e','i','o','u']
+#     permutations=itertools.permutations(letters)
+#     for perm in permutations:
+#         print(''.join(perm))
+#     generate_permutations()
+
+x=12
+y=12
+Z=x+y
+print(y)
